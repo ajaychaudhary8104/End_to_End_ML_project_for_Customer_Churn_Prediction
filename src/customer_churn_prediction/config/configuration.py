d3edc5df5287a388eac848d7dd685ed6a28da9e9
ajaychaudhary8955 <ajaychaudhary8104@gmail.com>
2026-05-21 00:06:22 +0530

    add automatic model promotion pipeline

--- src/customer_churn_prediction/config/configuration.py
+++ src/customer_churn_prediction/config/configuration.py
@@ -1,6 +1,12 @@
 from src.customer_churn_prediction.constants import *
 from src.customer_churn_prediction.utils.common import read_yaml, create_directories
-from src.customer_churn_prediction.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, DataPreprocessingConfig, ModelTrainingConfig, ModelEvaluationConfig
+from src.customer_churn_prediction.entity.config_entity import (DataIngestionConfig, 
+                                                                DataValidationConfig,
+                                                                  DataTransformationConfig,
+                                                                    DataPreprocessingConfig,
+                                                                      ModelTrainingConfig, 
+                                                                      ModelEvaluationConfig,
+                                                                      ModelPromotionConfig)
 
 
 class ConfigurationManager:
@@ -118,3 +124,25 @@ class ConfigurationManager:
         )
 
         return model_evaluation_config
+    
+
+    def get_model_promotion_config(self) -> ModelPromotionConfig:
+        config = self.config.model_promotion
+
+        create_directories([config.root_dir])
+
+        model_promotion_config = ModelPromotionConfig(
+            root_dir=config.root_dir,
+            metrics_file_path=config.metrics_file_path,
+            model_file_path=config.model_file_path,
+            production_model_path=config.production_model_path,
+            registered_model_name=config.registered_model_name,
+            target_stage=config.target_stage,
+            mlflow_uri=config.mlflow_uri,
+            promote_metric=config.promote_metric,
+            promote_threshold=float(config.promote_threshold),
+            archive_existing_versions=bool(config.archive_existing_versions),
+            copy_local_model=bool(config.copy_local_model)
+        )
+
+        return model_promotion_config
